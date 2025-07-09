c8d2406ad388e159733ea63083c99e488686da9b
Sudharasan21 <sudhardeveloper2124@gmail.com>
2025-07-09 17:46:05 +0530

    Connected frontend to backend on Render

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Mainpage from "./components/mainpage";
 
 // ✅ Log the backend URL for testing
-console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
-
 const API_URL = `${process.env.REACT_APP_BACKEND_URL}/todos`;
+console.log("API_URL:", API_URL); // ✅ For testing
+
+
 
 const App = () => {
   return (
